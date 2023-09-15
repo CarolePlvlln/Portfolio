8c559eb021abbb88ee85f1783b0c370a0e41a2b3
Carole Pelvillain <pelvillain.carole@live.fr>
2023-09-15 15:20:31 +0200

    Media queries 60%

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,49 +1,80 @@
-import { NavLink } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/images/logo.png";
 import head from "./header.scss";
-//import PropTypes from 'prop-types';
+import { useScroll } from "../UseScroll";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Header() {
+const Header = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
   return (
     <div className="Header" style={head}>
       <img src={logo} alt="logo" className="logo" />
-      <div className="navBar">
-        <ul>
-          <li>
-            <a to="/" className="navLink navLink-fade-up" href="#presentation">
-              Accueil
-            </a>
-          </li>
-          <li>
-            <a
-              to="Home/Services"
-              className="navLink navLink-fade-up"
-              href="#services"
-            >
-              Services
-            </a>
-          </li>
-          <li>
-            <a className="navLink navLink-fade-up" href="#travaux">
-              Travaux
-            </a>
-          </li>
-          <li>
-            <a className="navLink navLink-fade-up" href="#aPropos">
-              A Propos
-            </a>
-          </li>
-          <li>
-            <a className="navLink navLink-fade-up" href="#contact">
-              Contact
-            </a>
-          </li>
-        </ul>
+      <div className="menu">
+        <div
+          className="iconMenu toogle"
+          onClick={() => setNavbarOpen((prev) => !prev)}
+        >
+          {navbarOpen ? (
+            <FontAwesomeIcon
+              className="iconsMenu bar"
+              icon={faBars}
+              style={{ width: "30px", height: "30px" }}
+            />
+          ) : (
+            <FontAwesomeIcon
+              className="iconsMenu cross"
+              icon={faXmark}
+              style={{
+                width: "32px",
+                height: "32px",
+              }}
+            />
+          )}
+        </div>
+        <div className="navBar">
+          <ul>
+            <li>
+              <a
+                to="/"
+                className="navLink navLink-fade-up"
+                href="#presentation"
+              >
+                Accueil
+              </a>
+            </li>
+            <li>
+              <a
+                to="Home/Services"
+                className="navLink navLink-fade-up"
+                href="#services"
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a className="navLink navLink-fade-up" href="#travaux">
+                Travaux
+              </a>
+            </li>
+            <li>
+              <a className="navLink navLink-fade-up" href="#aPropos">
+                A Propos
+              </a>
+            </li>
+            <li>
+              <a className="navLink navLink-fade-up" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 Header.propTypes = {};
 
