97b88c4913b39f15ab4477189ea8e5053517e7c1
Carole Pelvillain <pelvillain.carole@live.fr>
2023-09-18 18:04:28 +0200

    menus media queries

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -75,6 +75,39 @@ const Header = () => {
           <></>
         )}
       </div>
+      <div className="navBarMain">
+        <ul>
+          <li>
+            <a to="/" className="navLink navLink-fade-up" href="#presentation">
+              Accueil
+            </a>
+          </li>
+          <li>
+            <a
+              to="Home/Services"
+              className="navLink navLink-fade-up"
+              href="#services"
+            >
+              Services
+            </a>
+          </li>
+          <li>
+            <a className="navLink navLink-fade-up" href="#travaux">
+              Travaux
+            </a>
+          </li>
+          <li>
+            <a className="navLink navLink-fade-up" href="#aPropos">
+              A Propos
+            </a>
+          </li>
+          <li>
+            <a className="navLink navLink-fade-up" href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
